2d45ceb67ee3221b26f1da46520bd7500fdcaf4e
Bartosz Kopacz <bbartekk93@gmail.com>
2023-11-02 19:29:12 +0100

    Program iterates through the elements in the manufacturing module and displays objects of type: folder, tool, workpiece, mcs, methods.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NXOpen;
+using NXOpen.CAM;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        NXOpen.Session theSession = NXOpen.Session.GetSession();
+		NXOpen.Part workPart = theSession.Parts.Work;
+		NXOpen.Part displayPart = theSession.Parts.Display;
+		
+        CAMSetup camSetup = workPart.CAMSetup;
+
+		NXOpen.CAM.NCGroupCollection groups = camSetup.CAMGroupCollection;
+		
+		foreach (NXOpen.CAM.NCGroup group in groups)
+        {
+            Type opType = group.GetType();
+            NXOpen.UF.UFSession ufsession = NXOpen.UF.UFSession.GetUFSession();
+			theSession.ListingWindow.Open();
+			theSession.ListingWindow.WriteLine(opType.ToString());
+			theSession.ListingWindow.Close();               
+        }     
+    }
+}
